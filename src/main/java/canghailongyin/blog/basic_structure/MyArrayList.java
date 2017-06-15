ba4b57943464723a2a8d46527531961b61960ac7
summerhai <minglaihan@163.com>
2017-06-15 18:18:52 +0800

    增加基本数据结构定义类，慢慢完善

---
+++ src/main/java/canghailongyin/blog/basic_structure/MyArrayList.java
@@ -0,0 +1,10 @@
+package canghailongyin.blog.basic_structure;
+
+/**
+ * Created by mingl on 2017-6-15.
+ * ArrayList是基于动态数组的实现，查找和修改快，但是插入和删除慢
+ * LinkedList是基于双链表的实现，插入和删除快，但是查找和修改慢
+ */
+
+public class MyArrayList {
+}
---
+++ src/main/java/canghailongyin/blog/basic_structure/MyLinkedList.java
@@ -0,0 +1,7 @@
+package canghailongyin.blog.basic_structure;
+
+/**
+ * Created by mingl on 2017-6-15.
+ */
+public class MyLinkedList {
+}
