46efb0261427fcfdeda7fb68764f570646cc5672
summerhai <minglaihan@163.com>
2017-09-15 13:12:57 +0800

    增加练习

--- src/main/java/canghailongyin/blog/leetcode/linkedlist/ResverseLinkList.java
+++ src/main/java/canghailongyin/blog/leetcode/linkedlist/ResverseLinkList.java
@@ -8,15 +8,15 @@ import canghailongyin.blog.basic_structure.ListNode;
  */
 public class ResverseLinkList {
 
-    public ListNode resverseLinkList(ListNode head){
-        if(head == null)
+    public ListNode resverseLinkList(ListNode head) {
+        if (head == null)
             return null;
         ListNode newHead = null;
         ListNode curNode = head;
         ListNode preNode = null;
-        while(curNode != null){
+        while (curNode != null) {
             ListNode nextNode = curNode.next;
-            if(nextNode == null){
+            if (nextNode == null) {
                 newHead = curNode;
             }
             curNode.next = preNode;
@@ -28,30 +28,35 @@ public class ResverseLinkList {
 
     /**
      * Given 1->2->3->4->5->NULL, m = 2 and n = 4,
-
-     return 1->4->3->2->5->NULL.
+     * <p>
+     * return 1->4->3->2->5->NULL.
+     *
      * @param head
      * @param start
      * @param end
      * @return
      */
-    public ListNode resverseLinkList(ListNode head,int start,int end){
-        if(head == null){
-            return null;
-        }
-        int count = 1;
-        ListNode newHead = null;
-        ListNode curNode = head;
-        ListNode preNode = null;
-        while(curNode != null){
-
-            if(count == start){//说明达到了要转换的点
-
-            }else{
-
+    public ListNode resverseLinkList(ListNode head, int start, int end) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode newHead = new ListNode(-1);
+        newHead.next = head;
+        ListNode beforeM = newHead;
+        ListNode node1 = null, node2 = null;
+        for (int i = 0; i < end; i++) {
+            if (i < start - 1) {
+                beforeM = beforeM.next;
+            } else if (i == start - 1) {
+                node1 = beforeM.next;
+                node2 = node1.next;
+            } else {
+                node1.next = node2.next;
+                node2.next = beforeM.next;
+                beforeM.next = node2;
+                node2 = node1.next;
             }
         }
-        return null;
+        return newHead.next;
 
     }
 
